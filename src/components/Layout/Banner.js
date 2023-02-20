9aed263cc965bebf3d5b1c85fc4102bb16a54cff
Said-laasri <saidlaasri3@gmail.com>
2023-02-20 18:25:45 +0100

    Add image Banner component

---
+++ src/components/Layout/Banner.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Image from "../../assets/meals.jpg";
+
+import classes from "./Header.module.css";
+
+const Banner = () => {
+  return (
+    <div className={classes["main-image"]}>
+      <img src={Image} alt="Eat to your full" />;
+    </div>
+  );
+};
+
+export default Banner;
