80fce21a940f6e4431a32a7ddee2e103c28070e4
Said-laasri <saidlaasri3@gmail.com>
2023-02-20 18:30:34 +0100

    Display meal list in page

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import DUMMY_MEALS from "./DummyMeals";
+import MealsItem from "./MealsItem/MealsItem";
+
+import Card from "../UI/Card";
+import classes from "./AvailableMeals.module.css";
+
+const AvailavleMeals = (props) => {
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealsItem
+      key={meal.id}
+      name={meal.name}
+      price={meal.price}
+      description={meal.description}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailavleMeals;
