a4c78e565e939c89944de269741cbdd96b8ceb32
Said-laasri <saidlaasri3@gmail.com>
2023-02-21 22:34:20 +0100

    Create hide and show methde

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsSohwn, setCartIsShown] = useState(false);
+
+  const showCarthundler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCarthundler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Header />
-      <Meals />
-      <Cart />
+      {cartIsSohwn && <Cart hideCart={hideCarthundler} />}
+      <Header  showCart={showCarthundler} />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
