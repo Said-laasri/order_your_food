6ae4906628e21e2eec17206f8f140305e66d020f
Said-laasri <saidlaasri3@gmail.com>
2023-02-21 22:35:38 +0100

    Fix error length 'auto' instead of width and heigth

--- src/components/Cart/CartIcon.js
+++ src/components/Cart/CartIcon.js
@@ -3,8 +3,7 @@ import React from "react";
 const CartIcon = () => {
   return (
     <svg
-      width="auto"
-      height="auto"
+      length="auto"
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
