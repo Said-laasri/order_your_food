c50395ec28bea6aa95ddc5522c3b73e0242e9943
Said-laasri <saidlaasri3@gmail.com>
2023-02-22 22:53:55 +0100

    Add number of item in cart dynamicaly

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useContext} from "react";
 
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
-    const {showCart} = props
+    const {showCart} = props;
+    const cartCox = useContext(CartContext);
+
+    const numCartItem = cartCox.items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0);
+
 
   return (
     <button onClick={showCart} className={classes.button}>
@@ -13,8 +20,7 @@ const HeaderCartButton = (props) => {
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>
-        1
+      <span className={classes.badge}>{numCartItem}
       </span>
     </button>
   );
