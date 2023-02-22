b934b57e03c0d8449d7f3b9d78449aa26f6eff09
Said-laasri <saidlaasri3@gmail.com>
2023-02-22 22:52:37 +0100

    Import CartProvider and wrap the component that will consime the state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsSohwn, setCartIsShown] = useState(false);
@@ -16,13 +17,13 @@ function App() {
   };
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsSohwn && <Cart hideCart={hideCarthundler} />}
       <Header  showCart={showCarthundler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
