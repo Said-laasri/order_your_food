d5a42a343e4302732a3c3a3de83c0b06de083808
Said-laasri <saidlaasri3@gmail.com>
2023-02-25 16:56:08 +0100

    Add submit hundler and useRef to acess the entredamout on input

--- src/components/Meals/MealsItem/MealAddForm.js
+++ src/components/Meals/MealsItem/MealAddForm.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 import Input from "../../UI/Input";
 
 import classes from "./MealAddForm.module.css";
 
 const MealAddform = (props) => {
+  const { onAddToCart } = props;
+  const amountInputRef = useRef();
+  const [amoutIsValid, setAmountIsValid] = useState(true);
+   
+  const submitHundler = (e) => {
+    e.preventDefault();
+
+    const entredAmout = amountInputRef.current.value;
+    const entredAmoutNumber = +entredAmout;
+   
+    if (
+      entredAmout.trim().length === 0 ||
+      entredAmoutNumber < 1 ||
+      entredAmoutNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    onAddToCart(entredAmoutNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHundler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount",
@@ -19,6 +42,7 @@ const MealAddform = (props) => {
         }}
       />
       <button>+ Add</button>
+      {!amoutIsValid && <p>Please enter a Valid number!</p>}
     </form>
   );
 };
