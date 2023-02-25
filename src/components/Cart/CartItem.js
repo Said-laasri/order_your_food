8ca7fa72f6ab057f7bd54d640d5ca257c2d28a5d
Said-laasri <saidlaasri3@gmail.com>
2023-02-25 16:43:49 +0100

    Add cart item to display data for each cart element

---
+++ src/components/Cart/CartItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import classes from "./CartItem.module.css";
+
+const CartItem = (props) => {
+  const { onRemove, onAdd, name, amount, price } = props;
+
+  return (
+    <li className={classes["cart-item"]}>
+      <div>
+        <h2>{name}</h2>
+        <div className={classes.summary}>
+          <span className={classes.price}>${price.toFixed(2)}</span>
+          <span className={classes.amount}>x {amount}</span>
+        </div>
+      </div>
+      <div className={classes.actions}>
+        <button onClick={onRemove}>−</button>
+        <button onClick={onAdd}>+</button>
+      </div>
+    </li>
+  );
+};
+
+export default CartItem;
