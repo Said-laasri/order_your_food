9a636d4be51304e15c75d67293381ed5cc041229
Said-laasri <saidlaasri3@gmail.com>
2023-02-25 16:57:37 +0100

    Use context to additem to cart by acessing the availabel mealList

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import Modal from "../UI/Modal";
-
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
+  const cartConx = useContext(CartContext);
+
   const { hideCart } = props;
 
+  const totalAmount = `$${cartConx.totalAmount.toFixed(2)}`;
+  const hasItems = cartConx.items.length > 0;
+
+  const AddToCartHundler = (item) => [
+    cartConx.addItem({ ...item, amount: totalAmount }),
+  ];
+
+  const removeFromCarthundler = (id) => {;
+  };
+
   const cartItems = (
-    <ul className={classes['cart-items']}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
-        <li key={item.id}>{item.name}</li>
+    <ul className={classes["cart-items"]}>
+      {cartConx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onAdd={AddToCartHundler.bind(null, item)}
+          onRemove={removeFromCarthundler.bind(null, item.id)}
+        />
       ))}
     </ul>
   );
@@ -20,11 +40,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-        <button onClick={hideCart} className={classes["button--alt"]}>Close</button>
-        <button className={classes.button}>Order</button>
+        <button onClick={hideCart} className={classes["button--alt"]}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
