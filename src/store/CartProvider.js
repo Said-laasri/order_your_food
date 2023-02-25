0992dfe270ee5bd416d936e7c2bd94fb8895744c
Said-laasri <saidlaasri3@gmail.com>
2023-02-25 17:08:14 +0100

    On add existing item update old item amount

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -2,20 +2,40 @@ import React, { useReducer } from "react";
 
 import CartContext from "./cart-context";
 
-const defaultState = {  
+const defaultState = {
   items: [],
-  totalAmount: 0
+  totalAmount: 0,
 };
 
 const CartReducer = (state, action) => {
   if (action.type === "ADD") {
-    const updatedItems = state.items.concat(action.item);
-    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
     };
   }
+  
   if (action.type === "REMOVE") {
     const updatedItems = state.items.filter((item) => item.id !== action.id);
     const updatedTotalAmount = state.totalAmount - action.item.price;
