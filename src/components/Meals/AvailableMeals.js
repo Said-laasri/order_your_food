e5a6b0dc626e3a50effd285767c77075300e2da5
Said-laasri <saidlaasri3@gmail.com>
2023-04-08 03:39:31 +0000

    Update list meal to come from Firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,13 +1,39 @@
-import React from "react";
-
-import DUMMY_MEALS from "./DummyMeals";
+import React, { useEffect, useState } from "react";
 import MealsItem from "./MealsItem/MealsItem";
-
+import Spinner from "../UI/Spinner";
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 
-const AvailavleMeals = (props) => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("https://order-food-1f7c5-default-rtdb.firebaseio.com/Meals.json")
+        .then((response) => response.json())
+        .then((data) => {
+          const loadedMeals = [];
+          for (const key in data) {
+            loadedMeals.push({
+              id: key,
+              name: data[key].name,
+              description: data[key].description,
+              price: data[key].price,
+              image: data[key].image,
+            });
+          }
+          setMeals(loadedMeals);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          // handle errors here
+          setIsLoading(false);
+        });
+    }, 2000);
+  }, []);
+
+  const mealsList = meals.map((meal) => (
     <MealsItem
       key={meal.id}
       id={meal.id}
@@ -21,10 +47,15 @@ const AvailavleMeals = (props) => {
   return (
     <section className={classes.meals}>
       <Card>
+        {isLoading && (
+          <div className={classes.center}>
+            <Spinner />
+          </div>
+        )}
         <ul>{mealsList}</ul>
       </Card>
     </section>
   );
 };
 
-export default AvailavleMeals;
+export default AvailableMeals;
